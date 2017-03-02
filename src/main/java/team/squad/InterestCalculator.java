6c2eb52d61260a5ae444ec78316d7d5918201ba6
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-02 11:09:49 -0500

    added simple json example post request

--- src/main/java/team/squad/InterestCalculator.java
+++ src/main/java/team/squad/InterestCalculator.java
@@ -20,6 +20,31 @@ public class InterestCalculator {
     private InterestType interestType;
     private CalculationRule calculationRule;
 
+    public Account getAccount() {
+        return account;
+    }
+
+    public Interval getInterval() {
+        return interval;
+    }
+
+    public Long getInterestAmount() {
+        interestAmount = 1337L;
+        return interestAmount;
+    }
+
+    public Interval getFrequency() {
+        return frequency;
+    }
+
+    public InterestType getInterestType() {
+        return interestType;
+    }
+
+    public CalculationRule getCalculationRule() {
+        return calculationRule;
+    }
+
     public void setAccount(Account account) {
         this.account = account;
     }
@@ -38,11 +63,6 @@ public class InterestCalculator {
 
     public void setCalculationRule(CalculationRule calculationRule) {
         this.calculationRule = calculationRule;
-    }
-
-    public Long getInterestAmount() {
-        interestAmount = 1337L;
-        return interestAmount;
     }
 
     public long calculateSimpleInterest() {
