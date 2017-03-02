2c118dd41958e75b0e97c8e8e54b46ee2c50d389
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-02 12:55:13 -0500

    only started the project, nothing else

---
+++ src/main/java/team/squad/InterestcalculatorApplication.java
@@ -0,0 +1,12 @@
+package team.squad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestcalculatorApplication.class, args);
+	}
+}
